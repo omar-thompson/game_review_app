e4947a92bc5cab9d4b2c6827749fff1aae554037
Omar Thompson <omarshakkaithompson@gmail.com>
2026-08-28 10:53:55 +1000

    GRP-11 Connected MongoDB database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +15,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Game Review API is running!");
 });
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
